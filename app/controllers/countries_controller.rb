f52cd323831f1e5ec8cc093d637abe376af8d057
JoelLindow <joellindow@gmail.com>
2017-11-02 16:59:20 -0600

    dream driving api consumption. will delete and test drive after success

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,6 +7,14 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
+
+    response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{@country.abbreviation}")
+
+    json_holidays = JSON.parse(response.body, symbolize_names: true)[:holidays]
+
+    @holidays = json_holidays.each do |holiday|
+      Holiday.new(holiday)
+    end
   end
 
 end
