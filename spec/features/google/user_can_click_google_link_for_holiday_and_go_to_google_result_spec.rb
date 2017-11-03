0ac90b53aaa7e2e7e2925c546288a548c8c0c4d1
JoelLindow <joellindow@gmail.com>
2017-11-03 08:19:26 -0600

    started google link testing

---
+++ spec/features/google/user_can_click_google_link_for_holiday_and_go_to_google_result_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "User visits page for a Holiday" do
+  it "User can click google link and be taken to search result" do
+    country = Country.create!(name: "Angola", abbreviation: "AO", flag_img_url: "http://www.lse.ac.uk/study/informationForInternationalStudents/countryRegion/africa/angola/angolanFlag.gif")
+
+    #Need Log In With Oauth Test
+
+
+  end
+end
