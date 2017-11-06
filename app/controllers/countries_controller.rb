8f0a1435d21c3eec4498a66bc986810631a713ff
JoelLindow <joellindow@gmail.com>
2017-11-06 15:35:10 -0700

    insanely overkilled refactor

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -38,8 +38,15 @@ end
 class HolidayApiService
 
   def get_holidays_by_country(country_abbreviation)
-    response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{country_abbreviation}")
+    json_parse(api_call(country_abbreviation))
+  end
 
+  def json_parse(response)
     JSON.parse(response.body, symbolize_names: true)[:holidays]
   end
+
+  def api_call(country_abbreviation)
+    Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{country_abbreviation}")
+  end
+
 end
