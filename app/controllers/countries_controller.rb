4fedc621bf6a7bcab443f956051f40d2259f8a00
JoelLindow <joellindow@gmail.com>
2017-11-06 15:06:24 -0700

    breaking out api call for holiday data to HolidayPresenter

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -6,7 +6,30 @@ class CountriesController < ApplicationController
   end
 
   def show
+    @holidays = HolidayPresenter.new(params[:id]).base_api_call
+
     @country = Country.find(params[:id])
+    #
+    # response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{@country.abbreviation}")
+    #
+    # json_holidays = JSON.parse(response.body, symbolize_names: true)[:holidays]
+    #
+    # @holidays = json_holidays.map do |holiday|
+    #   Holiday.new(holiday)
+    # end
+    #
+    @web_search = (@country.name.split(" ").join("+") + "+")
+  end
+end
+
+class HolidayPresenter
+
+  def initialize(country_id)
+    @country_id = country_id
+  end
+
+  def base_api_call
+    @country = Country.find(@country_id)
 
     response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{@country.abbreviation}")
 
@@ -16,7 +39,7 @@ class CountriesController < ApplicationController
       Holiday.new(holiday)
     end
 
-    @web_search = (@country.name.split(" ").join("+") + "+")
+    @holidays
   end
 
 end
