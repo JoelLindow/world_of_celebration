9e8a523894f91df7ec2d3f7cf7e0b7fef52dbf60
JoelLindow <joellindow@gmail.com>
2017-11-06 15:20:02 -0700

    refactor

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -25,32 +25,20 @@ end
 class HolidayPresenter
 
   def initialize(country_id)
-    @country_id = country_id
+    @country = Country.find(country_id)
   end
 
   def base_api_call
-    HolidayApiService.new.get_holidays(@country_id).map do |holiday|
+    HolidayApiService.new.get_holidays_by_country(@country.abbreviation).map do |holiday|
       Holiday.new(holiday)
     end
-
-    # country = Country.find(@country_id)
-    #
-    # response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{country.abbreviation}")
-    #
-    # json_holidays = JSON.parse(response.body, symbolize_names: true)[:holidays]
-    #
-    # @holidays = json_holidays.map do |holiday|
-    #   Holiday.new(holiday)
-    # end
   end
 end
 
 class HolidayApiService
 
-  def get_holidays(country_id)
-    country = Country.find(country_id)
-
-    response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{country.abbreviation}")
+  def get_holidays_by_country(country_abbreviation)
+    response = Faraday.get("https://holidayapi.com/v1/holidays?key=#{ENV["HOLIDAY_API_KEY"]}&year=2016&country=#{country_abbreviation}")
 
     JSON.parse(response.body, symbolize_names: true)[:holidays]
   end
