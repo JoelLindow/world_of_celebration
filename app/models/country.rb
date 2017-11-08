d4e04b4ad8e0243f58f94b5950196f115c709929
JoelLindow <joellindow@gmail.com>
2017-11-08 11:47:11 -0700

    add final validations to country model (which users can NOT touch)

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,5 @@
 class Country < ApplicationRecord
+
+  validates_presence_of :name, :abbreviation, :flag_img_url
+  
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
 describe Country do
-  it "can return attributes" do
+  it "is valid with all neccessary attributes" do
     country = Country.create!(name: "Argentina", abbreviation: "AR", flag_img_url: "https://cdn3.volusion.com/jmlfn.xfvpt/v/vspfiles/photos/argentina-flag-2.jpg?1433246627")
 
     expect(country.name).to eq("Argentina")
     expect(country.abbreviation).to eq("AR")
     expect(country.flag_img_url).to eq("https://cdn3.volusion.com/jmlfn.xfvpt/v/vspfiles/photos/argentina-flag-2.jpg?1433246627")
+    expect(country).to be_valid
+  end
+
+  it "is invalid without name" do
+    country = Country.create(abbreviation: "AR", flag_img_url: "https://cdn3.volusion.com/jmlfn.xfvpt/v/vspfiles/photos/argentina-flag-2.jpg?1433246627")
+
+    expect(country).to be_invalid
+  end
+
+  it "is invalid without abbreviation" do
+    country = Country.create(name: "Argentina", flag_img_url: "https://cdn3.volusion.com/jmlfn.xfvpt/v/vspfiles/photos/argentina-flag-2.jpg?1433246627")
+
+    expect(country).to be_invalid
+  end
+
+  it "is invalid without flag_img_url" do
+    country = Country.create(name: "Argentina", abbreviation: "AR")
+
+    expect(country).to be_invalid
   end
 end
